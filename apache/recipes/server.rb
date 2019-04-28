2f65a2465dcbc32174e5e8bc6a34c35f0412e53f
Jan Libo <mail@janlibo.net>
2019-04-28 02:17:12 -0700

    removed 2 from output

--- apache/recipes/server.rb
+++ apache/recipes/server.rb
@@ -1,7 +1,7 @@
 package 'httpd'
 
 file '/var/www/html/index.html' do
-  content '<h1>Hello World2 from Chef CookBook</h1>'
+  content '<h1>Hello World from Chef CookBook</h1>'
 end
 
 service 'httpd' do
