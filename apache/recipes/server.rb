8004ef69e6a6f7c9d823ddf3589152cabeaafd8d
Jan Libo <mail@janlibo.net>
2019-04-28 08:14:40 -0700

    added index.html template

--- apache/recipes/server.rb
+++ apache/recipes/server.rb
@@ -1,11 +1,7 @@
 package 'httpd'
 
-file '/var/www/html/index.html' do
-  content "
-<h1>Hello World from Chef CookBook</h1>\n
-<h2>Hostname: #{node['hostname']}</h2>\n
-<h2>IP: #{node['ipaddress']}</h2>\n
-"
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
 end
 
 service 'httpd' do
