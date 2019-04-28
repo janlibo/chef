0aba1a460b0d39f7453b858a750fd9885e2fe787
Jan Libo <mail@janlibo.net>
2019-04-28 07:20:29 -0700

    added host info and IP address to html output

--- apache/recipes/server.rb
+++ apache/recipes/server.rb
@@ -1,7 +1,11 @@
 package 'httpd'
 
 file '/var/www/html/index.html' do
-  content '<h1>Hello World from Chef CookBook</h1>'
+  content "
+<h1>Hello World from Chef CookBook</h1>\n
+<h2>Hostname: #{node['hostname']}</h2>\n
+<h2>IP: #{node['ipaddress']}</h2>\n
+"
 end
 
 service 'httpd' do
